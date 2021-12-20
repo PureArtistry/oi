e08960e42409091ecd4c37984a637c81be246e1d
David Sherriff <d.sherriff.81@gmail.com>
2021-12-20 18:47:16 +0000

    updated lyrics.rs to make it actually readable (still a bunch of other files to go through)

--- src/selectors/lyrics.rs
+++ src/selectors/lyrics.rs
@@ -1,19 +1,22 @@
 use crate::selectors::{default_output, get_vec};
 
 pub fn main(data: &scraper::Html, tty: &bool, w: usize) {
-    let mut x = get_vec(data, "div.i4J0ge");
+    let mut lyrics = get_vec(data, "div.i4J0ge");
     for _ in 0..4 {
-        x.pop();
+        lyrics.pop();
     }
-    let y = get_vec(data, "div.ujudUb.WRZytc.OULBYb");
-    let j = y.len();
-    let mut i = x.iter().position(|&r| r == y[(j - 1)]).unwrap();
-    for _ in 0..j {
-        x.remove(i);
-        i -= 1
+    let duplicate_lines = get_vec(data, "div.ujudUb.WRZytc.OULBYb");
+    let dups_len = duplicate_lines.len();
+    let mut lyrics_index = lyrics
+        .iter()
+        .position(|&r| r == duplicate_lines[(dups_len - 1)])
+        .unwrap();
+    for _ in 0..dups_len {
+        lyrics.remove(lyrics_index);
+        lyrics_index -= 1
     }
     match tty {
-        true => default_output(&x.join("\n"), w),
-        false => println!("{}", x.join("\n"))
+        true => default_output(&lyrics.join("\n"), w),
+        false => println!("{}", lyrics.join("\n"))
     }
 }
