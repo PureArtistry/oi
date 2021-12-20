54d3346fccb488cbdfde8fc5d26954fee23eb367
David Sherriff <d.sherriff.81@gmail.com>
2021-12-20 23:15:48 +0000

    improved readability of clock.rs

--- src/selectors/clock.rs
+++ src/selectors/clock.rs
@@ -6,21 +6,23 @@ use comfy_table::{
 use crate::selectors::get_vec;
 
 pub fn main(data: &scraper::Html, tty: &bool, w: usize) {
-    let x = get_vec(data, "div.vk_gy.vk_sh.card-section.sL6Rbf");
-    let mut l1: Vec<&str> = vec![];
-    let mut l2: Vec<&str> = vec![];
-    let mut y1 = x[3].to_string();
-    y1.remove(0);
-    l1.push(&y1);
-    l1.push(x[4]);
-    let mut y2 = x[6].to_string();
-    y2.remove(y2.len() - 1);
-    l1.push(&y2);
-    let mut y3 = x[9].to_string();
-    y3.remove(0);
-    y3.remove(y3.len() - 1);
-    y3.remove(y3.len() - 1);
-    l2.push(&y3);
+    let source = get_vec(data, "div.vk_gy.vk_sh.card-section.sL6Rbf");
+
+    let mut date_vec: Vec<&str> = vec![];
+    let mut day = source[3].to_string();
+    day.remove(0);
+    date_vec.push(&day);
+    date_vec.push(source[4]);
+
+    let mut timezone = source[6].to_string();
+    timezone.remove(timezone.len() - 1);
+    date_vec.push(&timezone);
+
+    let mut location = source[9].to_string();
+    location.remove(0);
+    location.remove(location.len() - 1);
+    location.remove(location.len() - 1);
+
     match tty {
         true => {
             let mut t = Table::new();
@@ -30,18 +32,18 @@ pub fn main(data: &scraper::Html, tty: &bool, w: usize) {
             if w >= 100 {
                 t.set_table_width(100);
             }
-            t.set_header(Row::from(vec![Cell::new(x[1])
+            t.set_header(Row::from(vec![Cell::new(source[1])
                 .set_alignment(CellAlignment::Center)
                 .add_attribute(Attribute::Bold)
                 .fg(Color::Green)]));
             t.add_row(Row::from(vec![
-                Cell::new(l1.join("")).set_alignment(CellAlignment::Center)
+                Cell::new(date_vec.join("")).set_alignment(CellAlignment::Center)
             ]));
-            t.add_row(Row::from(vec![Cell::new(l2[0])
+            t.add_row(Row::from(vec![Cell::new(location)
                 .set_alignment(CellAlignment::Center)
                 .fg(Color::DarkGrey)]));
             println!("{}", t)
         }
-        false => println!("{}\n{}\n{}", x[1], l1.join(""), l2[0])
+        false => println!("{}\n{}\n{}", source[1], date_vec.join(""), location)
     }
 }
