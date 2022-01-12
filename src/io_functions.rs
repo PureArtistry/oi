ae4aefb57ec455df9eaa48a9d5d6918ce52922e5
David Sherriff <d.sherriff.81@gmail.com>
2022-01-12 18:37:19 +0000

    updated deps to latest versions

--- src/io_functions.rs
+++ src/io_functions.rs
@@ -10,7 +10,6 @@ pub fn fetch(query: String, lang: String) -> Result<String, ureq::Error> {
         .set(
             "User-Agent",
             "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/5.15.2 Chrome/87.0.4280.144 Safari/537.36"
-            // "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0"
         )
         .query("q", &query)
         .query("hl", &lang)
